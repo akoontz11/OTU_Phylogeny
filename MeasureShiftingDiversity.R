219ebdaf08a7ef00939192b11826cbb13b940ca7
Austin Koontz <austinkoontz11@gmail.com>
2019-04-05 13:54:01 -0600

    Including code for measurement of shifting mpd values

---
+++ MeasureShiftingDiversity.R
@@ -0,0 +1,39 @@
+# %%% MEASUREMENT OF MPD SHIFTS %%%
+
+# 1. Change in site "ranking" (of least diverse -> most diverse, say) versus changes in delta value
+  # a. Obtain mpd values for each corresponding delta value for an individual site
+  # b. Get the average difference between mpd values for one delta value and mpd values for the next delta value
+  # c. Plot those averages over change in delta
+        # This will only be for a single site, though. For multiple sites, you'd have to use multiple lines on the graph
+
+# Generate a numeric vector of site rankings (at delta=1.0)
+baseline.rank <- as.numeric(rank(sort(sim.data$transforms$orig.transform[,10],decreasing = F)))
+# Generate a vector of names of these site rankings (at delta=1.0)
+baseline.names <- names(sort(sim.data$transforms$orig.transform[,10],decreasing = F)) 
+baseline.names
+# Generate a vector to capture mean changes between delta values
+meanRankChanges <- vector(length = length(deltas)-1)
+# Generate a vector to capture delta value differences
+deltaDifferences <- vector(length = length(deltas)-1)
+# For each value in the vector of delta values
+for(i in 1:length(deltas)-1){
+  # Get the names of sites sorted from lowest to highest
+  comparison <- names(sort(sim.data$transforms$orig.transform[,i],decreasing = F))
+  # Generate a numeric vector corresponding to how site rankings have changed from "baseline" delta value
+  # (delta=1.0) to "current" delta value (delta[i])
+  shifts <- match(baseline, comparison)
+  # Calculate changes between two different rankings
+  changes <- abs(shifts - baseline.rank)
+  # Calculate mean changes, and place into vector
+  meanRankChanges[i] <- mean(changes)
+  # Calculate difference between current delta value and baseline delta value (delta=1.0)
+  deltaDifferences[i] <- (deltas[i]-deltas[10])
+}
+
+meanRankChanges
+deltaDifferences
+
+plot(meanRankChanges ~ deltaDifferences)
+
+# 2. Measure Pearson correlation coefficient between the mpd values and the delta values
+
