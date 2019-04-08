88f8b45be5940a30f2664a5c9782060e54049417
Austin Koontz <austinkoontz11@gmail.com>
2019-04-08 10:54:26 -0600

    Fixing bug in 1st technique used to measure relationship between change in mpd ranking for a site and delta value

--- MeasureShiftingDiversity.R
+++ MeasureShiftingDiversity.R
@@ -3,8 +3,7 @@
 # 1. Change in site "ranking" (of least diverse -> most diverse, say) versus changes in delta value
   # a. Obtain mpd values for each corresponding delta value for an individual site
   # b. Get the average difference between mpd values for one delta value and mpd values for the next delta value
-  # c. Plot those averages over change in delta
-        # This will only be for a single site, though. For multiple sites, you'd have to use multiple lines on the graph
+  # c. Plot those averages over differences between "current" and "baseline" delta values
 
 # Generate a numeric vector of site rankings (at delta=1.0)
 baseline.rank <- as.numeric(rank(sort(sim.data$transforms$orig.transform[,10],decreasing = F)))
@@ -21,7 +20,7 @@ for(i in 1:length(deltas)-1){
   comparison <- names(sort(sim.data$transforms$orig.transform[,i],decreasing = F))
   # Generate a numeric vector corresponding to how site rankings have changed from "baseline" delta value
   # (delta=1.0) to "current" delta value (delta[i])
-  shifts <- match(baseline, comparison)
+  shifts <- match(baseline.names, comparison)
   # Calculate changes between two different rankings
   changes <- abs(shifts - baseline.rank)
   # Calculate mean changes, and place into vector
@@ -33,7 +32,9 @@ for(i in 1:length(deltas)-1){
 meanRankChanges
 deltaDifferences
 
-plot(meanRankChanges ~ deltaDifferences)
+plot(meanRankChanges ~ deltaDifferences);lines(deltaDifferences, meanRankChanges)
 
 # 2. Measure Pearson correlation coefficient between the mpd values and the delta values
+my.model <- lm(meanRankChanges ~ deltaDifferences)
+summary(my.model)
 
