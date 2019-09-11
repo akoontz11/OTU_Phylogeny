e120404409b7000897b179b6fd76d83fe4d30686
akoontz11 <austinkoontz11@gmail.com>
2019-09-11 15:23:19 -0600

    Updates to examples, so more comments.

--- MeasureShiftingDiversity.R
+++ MeasureShiftingDiversity.R
@@ -3,10 +3,7 @@
 # 1. Average absolute change in site "ranking" (least diverse -> most diverse) versus changes in delta value
 # 2. Measure Pearson correlation coefficient between the average change in site rankings and the delta values
 
-# Function needs to be able to accomodate both of the above metrics for all 3 "levels" of 
-# diversity (i.e. original, with populations, and with sequencing error)
-
-measureShifts <- function(orig.transform, deltas){
+measureRankingShifts <- function(orig.transform, deltas){
   # (This function assumes that the deltas vector is structured
   # such that delta = 1.0 is the 10th value in the vector, and values increment by 0.1)
   # 1. --BUILD VECTOR OF CHANGES IN STIE RANKING BY MPD--
@@ -38,26 +35,22 @@ measureShifts <- function(orig.transform, deltas){
   measures <- list(mpdRankingChanges = meanRankChanges, r = r.value, deltaDifferences = deltaDifferences)
   return(measures)
 }
+
 # Demonstration with simulated species-level transforms 
-orig.site <- measureShifts(sim.data$transforms$orig.transform,deltas)
+orig.site <- measureRankingShifts(sim.Results[[4]]$transforms$orig.transform,deltas)
 str(orig.site)
 # Plot changes in mpd ranking of sites versus changes in delta values, 
 plot(orig.site$mpdRankingChanges[11:29] ~ orig.site$deltaDifferences[11:29],ylim=c(0,17),xlim=c(0,2.2),main="Original phylogenies: changes in diversity versus changes in delta values",xlab="Differences between delta values",ylab="mpdRankChanges");lines(orig.site$deltaDifferences[11:29], orig.site$mpdRankingChanges[11:29],col=1);points(orig.site$mpdRankingChanges[1:10] ~ orig.site$deltaDifferences[1:10]);lines(orig.site$deltaDifferences[1:10], orig.site$mpdRankingChanges[1:10],col=2)
 
-# Demonstration with simulated population-level transforms
-intra.site <- measureShifts(sim.data$transforms$intra.transform,deltas)
+# Demonstration with simulated population-level transforms 
+intra.site <- measureRankingShifts(sim.Results[[4]]$transforms$intra.transform,deltas)
 str(intra.site)
 # Plot changes in mpd ranking of sites versus changes in delta values, 
-plot(intra.site$mpdRankingChanges[11:29] ~ intra.site$deltaDifferences[11:29],ylim=c(0,17),xlim=c(0,2.2),main="Intra- phylogenies: changes in diversity versus changes in delta values",xlab="Differences between delta values",ylab="mpdRankChanges");lines(intra.site$deltaDifferences[11:29], intra.site$mpdRankingChanges[11:29],col=1);points(intra.site$mpdRankingChanges[1:10] ~ intra.site$deltaDifferences[1:10]);lines(intra.site$deltaDifferences[1:10], intra.site$mpdRankingChanges[1:10],col=2)
-
-# Demonstration with simulated seq-level transforms
-seq.site <- measureShifts(sim.data$transforms$seq_err.transform,deltas)
-str(seq.site)
-# Plot changes in mpd ranking of sites versus changes in delta values, 
-plot(seq.site$mpdRankingChanges[11:29] ~ seq.site$deltaDifferences[11:29],ylim=c(0,17),xlim=c(0,2.2),main="Seq-error phylogenies: changes in diversity versus changes in delta values",xlab="Differences between delta values",ylab="mpdRankChanges");lines(seq.site$deltaDifferences[11:29], seq.site$mpdRankingChanges[11:29],col=1);points(seq.site$mpdRankingChanges[1:10] ~ seq.site$deltaDifferences[1:10]);lines(seq.site$deltaDifferences[1:10], seq.site$mpdRankingChanges[1:10],col=2)
+plot(intra.site$mpdRankingChanges[11:29] ~ intra.site$deltaDifferences[11:29],ylim=c(0,17),xlim=c(0,2.2),main="Intra phylogenies: changes in diversity versus changes in delta values",xlab="Differences between delta values",ylab="mpdRankChanges");lines(intra.site$deltaDifferences[11:29], intra.site$mpdRankingChanges[11:29],col=1);points(intra.site$mpdRankingChanges[1:10] ~ intra.site$deltaDifferences[1:10]);lines(intra.site$deltaDifferences[1:10], intra.site$mpdRankingChanges[1:10],col=2)
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Testing new function
+# 1. Average differences in mpd values at adjacent delta transform values versus changes in delta value
+# 2. Measure Pearson correlation coefficient between the average change in site rankings and the delta values
 
 measureShifts <- function(orig.transform, deltas){
   # (This function assumes that the deltas vector is structured
@@ -73,18 +66,27 @@ measureShifts <- function(orig.transform, deltas){
     div.changes[,i] <- orig.transform[,i+1] - orig.transform[,i]
     # Calculate the means of those differences, and pass that value into a vector
     mean.changes[i] <- mean(div.changes[,i])
-    }
+  }
   # 2. --MEASURE PEARSON CORRELATION COEFFICIENT BETWEEN MPD VALUES AND DELTA VALUES--
-  #r.value <- cor(meanRankChanges,deltaDifferences)
+  # Create a vector of equal length to the vector of means (of mpd changes)
+  small.deltas <- deltas[1:29]
+  # Calculate r.value
+  r.value <- cor(mean.changes, small.deltas)
   # Return two measures and vector of differences in delta values
-  measures <- list(meansOfDiversityChanges = mean.changes)
+  measures <- list(meansOfDiversityChanges = mean.changes, r = r.value, small.deltas = small.deltas)
   return(measures)
 }
 
+test.site <- measureShifts(sim.Results[[4]]$transforms$orig.transform, deltas)
+test.site$meansOfDiversityChanges
+
 test.site <- measureShifts(sim.data$transforms$orig.transform, deltas)
-deltas
+test.site$meansOfDiversityChanges
 
-small.deltas <- deltas[1:29]
-small.deltas
+plot(test.site$meansOfDiversityChanges ~ test.site$small.deltas)
 
-plot(test.site$meansOfDiversityChanges ~ small.deltas)
+lm(MPD ~ delta + MPD_true + n_spp, data=data)
+# This requires mpd measurements, delta values, mpd values at delta = 1.0, and the number of species present in the community
+# Is this for all sites, that is, MPD values for all sites, species for all sites? How is this structured? 
+
+# Afterwards, how to apply this to all simulation settings...
