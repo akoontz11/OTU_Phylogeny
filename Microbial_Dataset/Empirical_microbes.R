d959beb6a61a2b93b6edc2047cb5ce75ed63eee5
akoontz11 <austinkoontz11@gmail.com>
2020-03-18 15:46:21 -0600

    Included function for comparing site ranking shifts from original community

--- Microbial_Dataset/Empirical_microbes.R
+++ Microbial_Dataset/Empirical_microbes.R
@@ -1,16 +1,9 @@
-# Script for reading in Bonnie's microbial dataset
+# Script for analysis of Bonnie's fungal dataset from CR
 
 library(ape)
 setwd("/home/akoontz11/OTU_Phylogeny/Microbial_Dataset/")
 
-# Read in and reformat community matrices
-# Bacterial
-bact.comm <- as.matrix(read.csv2("CRbact_communitydat.csv", header=TRUE, sep=","))
-soil.cores <- bact.comm[,1]
-bact.comm <- apply(bact.comm[,-1],2,as.numeric)
-rownames(bact.comm) <- soil.cores
-sort(unique(c(bact.comm)))
-# Fungal
+# Read in and reformat fungal community matrix
 fung.comm <- as.matrix(read.csv2("CRfungi_communitydat.csv", header=TRUE, sep=","))
 soil.cores <- fung.comm[,1]
 fung.comm <- apply(fung.comm[,-1],2,as.numeric)
@@ -18,15 +11,48 @@ rownames(fung.comm) <- soil.cores
 colnames(fung.comm) <- gsub(pattern="X", replacement="", colnames(fung.comm))
 sort(unique(c(fung.comm)))
 
-# Read in phylogenetic trees (aligned with mafft, built with RAxML)
-# Bacterial
-# ...
-# Fungal
+# Read in fungal phylogenetic tree (aligned with mafft, built using RAxML)
 fung.phylo <- read.tree("phylos/fungal/RAxML_bestTree.repCRfung_Feb28_phy")
 # Prune tree of tips not present in the community matrix
 fung.phylo <- keep.tip(fung.phylo, colnames(fung.comm))
 plot(fung.phylo, show.tip.label=T)
 
+# Find a ranking of sites by diversity before transforming the phylogeny
+# (We use this as a vector to compare our mpd values against later)
+# Create comparative comm object
+fung.cc <- comparative.comm(fung.phylo, fung.comm, force.root = 0)
+# Calculate mean pairwise distance value (for each of 24 sites in community)
+fung.mpds <- .mpd(fung.cc, abundance.weighted=TRUE) 
 
+# Build vector of delta values
+deltas <- seq(0.1,3,by=0.1)
+# Transform, building a matrix of mpds with sites as rows and delta values as columns
+fung.test <- phy.d.transform.plot(fung.phylo, fung.comm, deltas)
 
+# Summary of relative changes in diversity across sites
+# Declare vector to receive average ranking changes
+fung.siteShifts <- vector(length=length(deltas))
+# Function for comparing site rankings at each delta value to original site rankings (prior to transformations)
+compare.shifts <- function(x,d){
+  # Generate a numeric vector of baseline site rankings (i.e. delta=1.0)
+  baseline.rank <- as.numeric(rank(sort(d,decreasing = F)))
+  # Generate a vector of names of these site rankings
+  baseline.names <- names(sort(d,decreasing = F))
+  # Generate a vector to capture mean changes between rankings
+  meanRankChanges <- vector(length = ncol(x))
+  for (i in 1:ncol(x)){
+    # Get the names of sites sorted from lowest to highest for the current column (delta value)
+    comparison <- names(sort(x[,i],decreasing = F))
+    # Generate a numeric vector corresponding to how site rankings have changed from "baseline" delta value
+    shifts <- match(baseline.names, comparison)
+    # Calculate absolute changes between two different rankings
+    changes <- abs(shifts - baseline.rank)
+    # Calculate the mean of the absolute changes, and place into vector
+    meanRankChanges[i] <- mean(changes)
+  }
+  return(meanRankChanges)
+}
 
+fung.siteShifts <- compare.shifts(fung.test, fung.mpds)
+fung.siteShifts
+plot(fung.siteShifts ~ deltas, xlab="Delta values", ylab="Mean difference in site rankings from original")
