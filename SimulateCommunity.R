caecb382fb5e86f08904d7145fb253641d178538
akoontz11 <austinkoontz11@gmail.com>
2021-04-12 11:34:48 -0600

    Updates to sim.comm

--- SimulateCommunity.R
+++ SimulateCommunity.R
@@ -138,8 +138,6 @@ test <- SimulateCommnunity(comm.spp=10, comm.size=10, comm.birth=0.5, comm.death
                            intra.death=0.1,intra.steps=3,seq.birth=0.5,
                            seq.death=0.1,seq.steps=3)
 
-plot(test$phylogenies$orig.phylo)
-
 # %%% Simulate communities using sim.meta.phy.comm %%%----
 SimulateCommnunity <- function(comm.size,comm.spp,comm.timesteps,comm.migrate,comm.env,comm.abund,comm.stoch,comm.speciate,intra.birth,intra.death,intra.steps,seq.birth,seq.death,seq.steps){
   # Simulate community (using pez function sim.meta.phy.comm)----
