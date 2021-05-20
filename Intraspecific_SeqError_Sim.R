daf4c1fab5918615a9811780b077f86748d08b50
akoontz11 <austinkoontz11@gmail.com>
2021-05-20 12:55:22 -0600

    Split up branch ratio values

--- Intraspecific_SeqError_Sim.R
+++ Intraspecific_SeqError_Sim.R
@@ -6,7 +6,7 @@ library(phytools)
 
 # New function, rescaling tree----
 # Append trees onto phylogeny tips. birth, death, steps are fed to sim.bdtree. type determines populations or seq. error
-add.branch <- function(tree,birth,death,tree.ratio, type=c("pops","seq.err")){
+add.branch <- function(tree, birth, death, branch.ratio, type=c("pops","seq.err")){
   type <- match.arg(type)
   # If argument `tree` is without branches (i.e. NULL), return a tree that is NULL. Otherwise, add branches to it.
   if(is.null(tree)){
@@ -21,22 +21,8 @@ add.branch <- function(tree,birth,death,tree.ratio, type=c("pops","seq.err")){
       orig.tip <- orig.tree$tip.label[i]
       # Generate a new tree to be added onto the tip of the original tree, based on arguments
       new.tree <- sim.bdtree(b=birth, d=death, stop="time", t=1)
-      # Scale the resulting branches based on the values specified in the tree ratio vector
-      if(type=="seq.err"){
-        new.tree$edge.length <- new.tree$edge.length * (tree.ratio[3])
-        # if(max(branching.times(new.tree)) < 1){
-        #   new.tree$edge.length <- new.tree$edge.length * (tree.ratio[3]/max(new.tree$edge.length))
-        # } else {
-        #   new.tree$edge.length <- new.tree$edge.length * (tree.ratio[3]/max(branching.times(new.tree)))
-        # }
-      } else {
-        new.tree$edge.length <- new.tree$edge.length * (tree.ratio[2])
-        # if(max(branching.times(new.tree)) < 1){
-        #   new.tree$edge.length <- new.tree$edge.length * (tree.ratio[2]/max(new.tree$edge.length))
-        # } else {
-        #   new.tree$edge.length <- new.tree$edge.length * (tree.ratio[2]/max(branching.times(new.tree)))
-        # }
-      }
+      # Scale the resulting branches based on the branch ratio value specified
+      new.tree$edge.length <- new.tree$edge.length * (branch.ratio)
       # Rename new tree tip labels
       # Get number of tips of new tree
       new.tips <- Ntip(new.tree)
@@ -44,10 +30,10 @@ add.branch <- function(tree,birth,death,tree.ratio, type=c("pops","seq.err")){
       for (j in 1:new.tips){
         cur.tip <- new.tree$tip.label[j]
         # The `type` argument denotes whether to add a "p" or an "e" to the new tips, for populations or seq. error.
-        if(type=="seq.err"){
-          cur.tip <- sub("s","e",cur.tip)
-        } else{
+        if(type=="pops"){
           cur.tip <- sub("s","p",cur.tip)
+        } else{
+          cur.tip <- sub("s","e",cur.tip)
         }
         new.name <- paste0(orig.tip,"--",cur.tip)
         # Add that new name to the tip of the new tree
