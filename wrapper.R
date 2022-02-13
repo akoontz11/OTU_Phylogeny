6dd0f9d3fd99e62df33847973ac5ce2a639b26ce
akoontz11 <austinkoontz11@gmail.com>
2022-02-13 16:23:55 -0600

    Updates to command order

--- wrapper.R
+++ wrapper.R
@@ -13,8 +13,56 @@ deltas <- seq(0.1,3,by=0.1)
 # Simulate community function
 source("~/OTU_Phylogeny/SimulateCommunity.R")
 
-# Ultrametric-only scenarios--------------------------------------------------
+# Non-ultrametric/ultrametric, measuring SESmpd----
+
+# %%% Non-ultrametric params %%%
+# Varying initial species, intra/seq birth/death rates, and branch ratios
+params <- data.frame(expand.grid(comm.spp=c(50,100,200), 
+                                 intra.birth=seq(0.3,0.5,0.1), intra.death=seq(0.1,0.2,0.1),
+                                 seq.birth=seq(0.3,0.5,0.1), seq.death=seq(0.1,0.2,0.1),
+                                 intra.ratio=seq(0,1,.2), seq.ratio=seq(0,1,.2)))
+# Scale seq error, to make only as large as intra variation
+params$seq.ratio <- params$intra.ratio * params$seq.ratio
+# Subsetting to only include instances in which birth >= death parameters
+# params <- subset(params, intra.birth >= intra.death)
+# params <- subset(params, seq.birth >= seq.death)
+
+# Running simulation
+sim.Results <- mcMap(function(i) SimulateCommunity(params$comm.spp[i], comm.size=50,
+                                                   inter.birth=1, inter.death=0, 
+                                                   params$intra.birth[i], params$intra.death[i],
+                                                   params$seq.birth[i], params$seq.death[i],
+                                                   inter.ratio=25, 
+                                                   params$intra.ratio[i], params$seq.ratio[i]),
+                     1:nrow(params), mc.cores=24)
+
+# %%% Ultrametric params %%%
+# Varying initial species, birth rates, and branch ratios
+ultra.params <- data.frame(expand.grid(comm.spp=c(50,100,200),
+                                       intra.birth=seq(0.1,0.3,0.1), seq.birth=seq(0.1,0.3,0.1),
+                                       intra.ratio=seq(0,1,.2), seq.ratio=seq(0,1,.2)))
+# Scale seq error, to make only as large as intra variation
+ultra.params$seq.ratio <- ultra.params$intra.ratio * ultra.params$seq.ratio
+
+
+# Running simulation
+sim.ultra.Results <- mcMap(function(i) SimulateCommunity(ultra.params$comm.spp[i],comm.size=50,
+                                                         inter.birth=1,inter.death=0,
+                                                         intra.birth=ultra.params$intra.birth[i],
+                                                         intra.death=0,
+                                                         seq.birth=ultra.params$seq.birth[i],
+                                                         seq.death=0,
+                                                         inter.ratio=25,
+                                                         intra.ratio=ultra.params$intra.ratio[i],
+                                                         seq.ratio=ultra.params$seq.ratio[i]),
+                           1:nrow(ultra.params), mc.cores=24)
+
+# Saving results
+save.image("simResults/simResults_20210520.RData")
+
+# Old parameters: ultrametric/non-ultrametric, measuring MPD----
 # 
+# %%% Ultrametric scenarios %%%
 # # Varying intraspecific/sequencing error birth/death rates, and number of community species
 # params <- data.frame(expand.grid(comm.spp=c(5,10,15),intra.birth=seq(0.1,0.5,0.1),intra.death=seq(0.1,0.5,0.1),seq.birth=seq(0.1,0.5,0.1),seq.death=seq(0.1,0.5,0.1)))
 # 
@@ -32,7 +80,7 @@ source("~/OTU_Phylogeny/SimulateCommunity.R")
 # # Saving results
 # save.image("simResults/simResults.ULTRA_ONLY.RData")
 
-# (Non-)ultrametric scenarios--------------------------------------------------
+# %%% (Non-)ultrametric scenarios %%%
 # 
 # # Parameter set for ultrametric phylogenies (no positive death rates)
 # ultra.params <- data.frame(expand.grid(comm.spp=c(5,10,15),
@@ -66,50 +114,3 @@ source("~/OTU_Phylogeny/SimulateCommunity.R")
 # 
 # 
 # save.image("simResults/simResults.RData")
-
-# (Non-)ultrametric scenarios, SESmpd--------------------------------------------------
-
-# Non-ultrametric params
-# Varying initial species, intra/seq birth/death rates, and branch ratios
-params <- data.frame(expand.grid(comm.spp=c(50,100,200), 
-                                 intra.birth=seq(0.3,0.5,0.1), intra.death=seq(0.1,0.2,0.1),
-                                 seq.birth=seq(0.3,0.5,0.1), seq.death=seq(0.1,0.2,0.1),
-                                 intra.ratio=seq(0,1,.2), seq.ratio=seq(0,1,.2)))
-# Scale seq error, to make only as large as intra variation
-params$seq.ratio <- params$intra.ratio * params$seq.ratio
-# Subsetting to only include instances in which birth >= death parameters
-# params <- subset(params, intra.birth >= intra.death)
-# params <- subset(params, seq.birth >= seq.death)
-
-# Running simulation
-sim.Results <- mcMap(function(i) SimulateCommunity(params$comm.spp[i], comm.size=50,
-                                                   inter.birth=1, inter.death=0, 
-                                                   params$intra.birth[i], params$intra.death[i],
-                                                   params$seq.birth[i], params$seq.death[i],
-                                                   inter.ratio=25, 
-                                                   params$intra.ratio[i], params$seq.ratio[i]),
-                     1:nrow(params), mc.cores=24)
-
-# Ultrametric params
-# Varying initial species, birth rates, and branch ratios
-ultra.params <- data.frame(expand.grid(comm.spp=c(50,100,200),
-                                       intra.birth=seq(0.1,0.3,0.1), seq.birth=seq(0.1,0.3,0.1),
-                                       intra.ratio=seq(0,1,.2), seq.ratio=seq(0,1,.2)))
-# Scale seq error, to make only as large as intra variation
-ultra.params$seq.ratio <- ultra.params$intra.ratio * ultra.params$seq.ratio
-
-
-# Running simulation
-sim.ultra.Results <- mcMap(function(i) SimulateCommunity(ultra.params$comm.spp[i],comm.size=50,
-                                                         inter.birth=1,inter.death=0,
-                                                         intra.birth=ultra.params$intra.birth[i],
-                                                         intra.death=0,
-                                                         seq.birth=ultra.params$seq.birth[i],
-                                                         seq.death=0,
-                                                         inter.ratio=25,
-                                                         intra.ratio=ultra.params$intra.ratio[i],
-                                                         seq.ratio=ultra.params$seq.ratio[i]),
-                           1:nrow(ultra.params), mc.cores=24)
-
-# Saving results
-save.image("simResults/simResults_20210520.RData")
